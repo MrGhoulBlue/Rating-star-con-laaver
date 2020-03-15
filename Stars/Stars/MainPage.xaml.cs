5aa6b01debb3d3b902995c32afa3ccc22d2542d0
ed <pushpoped@gmail.com>
2020-03-14 20:04:14 -0600

    first commit

---
+++ Stars/Stars/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using LaavorRatingConception;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Stars
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RatingConception_Voted(object sender, EventArgs e)
+        {
+            RatingConception rating = (RatingConception)sender;
+            int index = rating.IndexVoted;
+            int value = rating.Value;
+
+            index_star.Text = index.ToString();
+            value_star.Text = value.ToString();
+
+            // rating.InitialValue = 2; sobres así se asigna por default e buen rating
+        }
+    }
+}
